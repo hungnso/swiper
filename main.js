1612b06e846a4b6f10b86e650dae0b839552cbb9
hungdv2 <hungndv2@unikom.vn>
2023-05-10 15:52:18 +0700

    update

--- main.js
+++ main.js
@@ -16,10 +16,59 @@ const swiperCards = new Swiper(".gallery-cards", {
 	}
 });
 
-let swiperThumbs = new Swiper(".gallery-thumbs", {
+let swiperThumbs = new Swiper(".swiper-list", {
 	
-     
       slidesPerView: 5,
 });
+const buttonShow = document.querySelector('.show-detail')
+const detail = document.querySelector('.gallery__overflow')
+let screenWidth = window.innerWidth
+console.log(screenWidth)
+function handleShowDetail() {
+	console.log('Button đã được click!');
+	detail.style.bottom = '55%'
+	if(screenWidth < 390){
+		detail.style.bottom = '65%'
+	} 
+	
+	// Các hành động khác bạn muốn thực hiện khi button được click
+  }
+
+
+buttonShow.addEventListener('click', handleShowDetail)
 
 // swiperThumbs.controller.control = swiperCards;
+
+/// Danh sách các filter 
+// const listFilter = ['Collection', 'Material', 'Shape', 'Tags'];
+const listFilter = [
+	{
+	name: "Collection",
+	active: false
+	},
+	{
+	name: "Material",
+	active: true
+	},
+	{
+	name: "Shape",
+	active: false
+	},
+	{
+	name: "Tags",
+	active: false
+	},
+];
+const listFilterContainer =  document.querySelector('.list-filter')
+
+listFilter.forEach(item => {
+	const filterItem = document.createElement('div')
+	filterItem.classList.add('filter-item', 'col-3', 'text-center')
+	if(item.active){
+		filterItem.classList.add('active-filter')
+	}
+	filterItem.textContent = item.name
+	listFilterContainer.appendChild(filterItem)
+
+})
+// const test = document.querySelector('.gallery__overflow')
